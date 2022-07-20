bb4dd1b4ccb0d0b6768c7f13b50d25a9565c2994
ogawatti <ogawattim@gmail.com>
2022-07-20 09:48:29 +0900

    add access-control-allow-origin header

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   await app.listen(process.env.APP_PORT || 3000);
 }
