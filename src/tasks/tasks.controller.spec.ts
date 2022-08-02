dbe0252fcafded66f94315e82d7e04ec0d284175
ogawatti <ogawattim@gmail.com>
2022-08-02 11:57:57 +0900

    fix specs

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,17 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from '../data-source';
+import { Task } from './entities/task.entity';
 import { TasksController } from './tasks.controller';
+import { TasksModule } from './tasks.module';
 import { TasksService } from './tasks.service';
 
 describe('TasksController', () => {
   let controller: TasksController;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
+      imports: [
+        TasksModule,
+        TypeOrmModule.forRoot(DataSourceOptions),
+        TypeOrmModule.forFeature([Task]),
+      ],
       controllers: [TasksController],
       providers: [TasksService],
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
+  });
+
+  afterEach(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
--- src/tasks/tasks.service.spec.ts
+++ src/tasks/tasks.service.spec.ts
@@ -1,15 +1,29 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from '../data-source';
+import { Task } from './entities/task.entity';
+import { TasksModule } from './tasks.module';
 import { TasksService } from './tasks.service';
 
 describe('TasksService', () => {
   let service: TasksService;
+  let module: TestingModule;
 
   beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
+    module = await Test.createTestingModule({
+      imports: [
+        TasksModule,
+        TypeOrmModule.forRoot(DataSourceOptions),
+        TypeOrmModule.forFeature([Task]),
+      ],
       providers: [TasksService],
     }).compile();
 
     service = module.get<TasksService>(TasksService);
+  });
+
+  afterEach(async () => {
+    await module.close();
   });
 
   it('should be defined', () => {
