b5380ef4732aabe77a601ba679d1b6c60e556c88
ogawatti <ogawattim@gmail.com>
2022-08-02 13:27:11 +0900

    add tasks controller spec

--- src/tasks/tasks.controller.spec.ts
+++ src/tasks/tasks.controller.spec.ts
@@ -1,6 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DataSourceOptions } from '../data-source';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskDto } from './dto/update-task.dto';
 import { Task } from './entities/task.entity';
 import { TasksController } from './tasks.controller';
 import { TasksModule } from './tasks.module';
@@ -9,6 +11,7 @@ import { TasksService } from './tasks.service';
 describe('TasksController', () => {
   let controller: TasksController;
   let module: TestingModule;
+  let service: TasksService;
 
   beforeEach(async () => {
     module = await Test.createTestingModule({
@@ -22,13 +25,81 @@ describe('TasksController', () => {
     }).compile();
 
     controller = module.get<TasksController>(TasksController);
+    service = module.get<TasksService>(TasksService);
   });
 
   afterEach(async () => {
     await module.close();
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  const task = new Task();
+  task.id = 1;
+  task.content = 'test';
+  const taskId = String(task.id);
+
+  describe('create', () => {
+    it('should return a task', async () => {
+      const spy = jest
+        .spyOn(service, 'create')
+        .mockImplementation(() => Promise.resolve(task));
+      const createTaskDto = new CreateTaskDto();
+      createTaskDto.content = 'test';
+
+      expect(await controller.create(createTaskDto)).toBe(task);
+      expect(spy).toHaveBeenCalledWith(createTaskDto);
+      spy.mockRestore();
+    });
+  });
+
+  describe('findAll', () => {
+    it('should return an array of tasks', async () => {
+      const result = [task];
+      const spy = jest
+        .spyOn(service, 'findAll')
+        .mockImplementation(() => Promise.resolve(result));
+
+      expect(await controller.findAll()).toBe(result);
+      expect(spy).toHaveBeenCalled();
+      spy.mockRestore();
+    });
+  });
+
+  describe('findOne', () => {
+    it('should return a tasks', async () => {
+      const spy = jest
+        .spyOn(service, 'findOne')
+        .mockImplementation(() => Promise.resolve(task));
+
+      expect(await controller.findOne(taskId)).toBe(task);
+      expect(spy).toHaveBeenCalledWith(task.id);
+      spy.mockRestore();
+    });
+  });
+
+  describe('update', () => {
+    it('should return a tasks', async () => {
+      const spy = jest
+        .spyOn(service, 'update')
+        .mockImplementation(() => Promise.resolve(task));
+      const updateTaskDto = new UpdateTaskDto();
+      updateTaskDto.content = 'test';
+      updateTaskDto.done = true;
+
+      expect(await controller.update(taskId, updateTaskDto)).toBe(task);
+      expect(spy).toHaveBeenCalledWith(task.id, updateTaskDto);
+      spy.mockRestore();
+    });
+  });
+
+  describe('remove', () => {
+    it('should not return body', async () => {
+      const spy = jest
+        .spyOn(service, 'remove')
+        .mockImplementation(() => Promise.resolve(true));
+
+      expect(await controller.remove(taskId)).toBe(undefined);
+      expect(spy).toHaveBeenCalledWith(task.id);
+      spy.mockRestore();
+    });
   });
 });
